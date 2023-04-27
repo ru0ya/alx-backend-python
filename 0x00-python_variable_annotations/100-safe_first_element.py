843628aeaacc008b4e48da3587e577c1f3776a97
ru0ya <viktaangi02@gmail.com>
2023-04-27 17:18:53 +0300

    debug

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,10 +2,10 @@
 """duck typing-first element of a sequence"""
 
 
-from typing import Sequence, List, Any, Union
+from typing import Sequence, Any, Union, NoneType
 
 
-def safe_first_element(lst: Sequence[Any]) -> List[Union[Any, None]]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
     """if a list returns the first element else returns none"""
     if lst:
         return lst[0]
